430616d855da4d109dcf74106baa62d01c3bbb36
Olha Yarosh <yarosholha1@gmail.com>
2022-09-06 13:48:32 +0300

    Added an icon to the title.

--- src/Box/Header/Header.js
+++ src/Box/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import classes from './Header.module.css'
+import {FaMoneyBillAlt} from 'react-icons/fa'
 
 const Header = () => {
   return (
     <div className={classes.heading}>
-      <div className={classes.title}>Currency Converter</div>
+      <div className={classes.title}>
+      <FaMoneyBillAlt className={classes.iconMoney}/>
+      Currency Converter</div>
     </div>
   )
 }
