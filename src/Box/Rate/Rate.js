719f5cf75896aced61a4435ba89c795553abcdce
Olha Yarosh <yarosholha1@gmail.com>
2022-09-06 16:28:22 +0300

    Divided the container into parts, added a button.

--- src/Box/Rate/Rate.js
+++ src/Box/Rate/Rate.js
@@ -7,12 +7,12 @@ const Rate = () => {
         <div className={classes.containerRate}>
             <div className={classes.currentRateBlock}>Date:<br/>Rate:</div>
             <div className={classes.givenCurrencyBlock}>
-                <select className={classes.selectGivenCurrency}></select>
-                <input className={classes.inputGiven}></input>
+                <div><select className={classes.selectGivenCurrency}></select></div>
+                <input type='number' className={classes.inputGiven}></input>
             </div>
             <div className={classes.returnedCurrencyBlock}>
-                <select className={classes.selectReturnedCurrency}></select>
-                <input className={classes.inputReturned}></input>
+                <div><select className={classes.selectReturnedCurrency}></select></div>
+                <input type='number' className={classes.inputReturned}></input>
             </div>
             <button className={classes.buttonConvert}>CONVERT</button>
         </div>
