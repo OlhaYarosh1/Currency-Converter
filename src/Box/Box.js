293050d2f185e7114fe3042471199711b956a62f
Olha Yarosh <yarosholha1@gmail.com>
2022-09-08 01:51:19 +0300

    Added currencies to selectors.

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Box.module.css'
 import Header from './Header/Header'
-import Rate from './Rate/Rate'
+import Rate from '../Rate/Rate'
 
 const Box = () => {
 
--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import classes from './Rate.module.css'
 
 const BASE_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 
 const Rate = () => {
 
-    const select = document.querySelectorAll('.currency');
         fetch(BASE_URL)
             .then(data => data.json())
             .then((data) => {
@@ -14,23 +13,19 @@ const Rate = () => {
 
         function display(data) {
             for(var i = 0; i < data.length; i++) {
-                console.log(data[i]['cc']);
-                select.innerHTML += `<option value='${data[i]['cc']}'>${data[i]['cc']}</option>`;
+                const selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[i]['cc']}'>${data[i]['cc']}</option>`;
+                const selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[i]['cc']}'>${data[i]['cc']}</option>`;
             }
         }
 
     return (
         <div className={classes.containerRate}>
             <div className={classes.fromCurrencyBlock}>
-                    <select name='currency' className={classes.currency}>
-                        <option value=''></option>
-                    </select>
                 <input type='number' className={classes.inputFrom}></input>
+                <select id='selectFrom' className={classes.currency}></select>
             </div>
             <div className={classes.toCurrencyBlock}>
-                    <select name='currency' className={classes.currency}>
-                        <option value=''></option>
-                    </select>
+                <select id='selectTo' className={classes.currency}></select>
                 <input type='number' className={classes.inputTo}></input>
             </div>
             <div className={classes.equals}>=</div>
