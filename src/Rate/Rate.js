8fed804d24e103c0b87ee72c15ab65bb2f99aae5
Olha Yarosh <yarosholha1@gmail.com>
2022-09-12 13:27:10 +0300

    Currencies with input.

--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -7,33 +7,41 @@ fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
         display(data)
     })
 
-function display(data) {
-        const selectFromUAH = document.getElementById('selectFrom').innerHTML += `<option value='UAH'>UAH</option>`;
-        const selectToUAH = document.getElementById('selectTo').innerHTML += `<option value='UAH'>UAH</option>`;
-        const selectFromUSD = document.getElementById('selectFrom').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
-        const selectToUSD = document.getElementById('selectTo').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
-        const selectFromGBP = document.getElementById('selectFrom').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
-        const selectToGBP = document.getElementById('selectTo').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
-        const selectFromEUR = document.getElementById('selectFrom').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
-        const selectToEUR = document.getElementById('selectTo').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
+    let selectFrom = document.getElementById('selectFrom')
+    let selectTo = document.getElementById('selectTo')
 
+
+function display(data) {
+
+    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='UAH'>UAH</option>`;
+    selectTo = document.getElementById('selectTo').innerHTML += `<option value='UAH'>UAH</option>`;
+    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
+    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
+    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
+    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
+    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
+    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
 }  
+
 
 const Rate = () => {
 
     return (
         <div className={classes.containerRate}>
+            <div className={classes.rate}>
+                <div className={classes.rate1}></div>
+                <div className={classes.rate2}></div>
+            </div>
             <div className={classes.fromCurrencyBlock}>
-                <input type='number' className={classes.inputFrom}></input>
-                <select id='selectFrom' className={classes.currency}></select>
+                <input id='inputFrom' placeholder='0' type='number' className={classes.inputFrom}></input>
+                <select id='selectFrom' className={classes.selectFrom}></select>
             </div>
             <div className={classes.toCurrencyBlock}>
-                <select id='selectTo' className={classes.currency}></select>
-                <input type='number' className={classes.inputTo}></input>
+                <input id='inputTo' placeholder='0' type='number' className={classes.inputTo}></input>
+                <select id='selectTo' className={classes.selectTo}></select>
             </div>
             <div className={classes.equals}>=</div>
-            <button className={classes.arrows}></button>
-            <button className={classes.buttonConvert}>CONVERT</button>
+            <button className={classes.buttonConvert} id='btn'>CONVERT</button>
         </div>
     )
 }
