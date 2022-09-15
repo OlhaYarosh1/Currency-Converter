5bfcbfde07baf13d8d2f9eab6cae0f07c1487c5b
Olha Yarosh <yarosholha1@gmail.com>
2022-09-16 01:17:02 +0300

    Changed colors, began complement blocks for the current exchange rates.

--- src/Box/Header/Header.js
+++ src/Box/Header/Header.js
@@ -6,10 +6,36 @@ const Header = () => {
     return (
         <div>
             <div className={classes.rateUSD}>
-                <div className={classes.usdBox}></div>
+                <div className={classes.usdBox}>
+                    <div className={classes.stringUSD}>
+                        1 USD is equivalent to 
+                    </div>
+                    <div className={classes.currentRateUSD}>
+                        Rate
+                    </div>
+                    <div className={classes.currentDate}>
+                        Dt.Mn.Year
+                    </div>
+                    <div className={classes.rateUAH}>
+                        UAH
+                    </div>
+                </div>
             </div>
             <div className={classes.rateEUR}>
-                <div className={classes.eurBox}></div>
+                <div className={classes.eurBox}>
+                    <div className={classes.stringEUR}>
+                        1 EUR is equivalent to 
+                    </div>
+                    <div className={classes.currentRateEUR}>
+                        Rate
+                    </div>
+                    <div className={classes.currentDate}>
+                        Dt.Mn.Year
+                    </div>
+                    <div className={classes.rateUAH}>
+                        UAH
+                    </div>
+                </div>
             </div>
         </div>
     )
