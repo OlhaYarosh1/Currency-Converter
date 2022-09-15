f67dce928fb694826d06e876caad387a2df36cc6
Olha Yarosh <yarosholha1@gmail.com>
2022-09-16 00:19:40 +0300

    Converts currency in both directions.

--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -1,56 +1,77 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import classes from './Rate.module.css'
-
-fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-    .then(data => data.json())
-    .then((data) => {
-        display(data)
-    })
-
-    let selectFrom = document.getElementById('selectFrom')
-    let selectTo = document.getElementById('selectTo')
-
-    let json = null;
-    let rates = null;
-function display(data) {
-    json = data;
-
-    rates = json.filter((item) => {
-        if (item.cc === 'USD' || item.cc === 'EUR' || item.cc === 'GBP') {
-            return true;
-        }
-        return false;
-    });
-
-    console.log(rates)
-    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='UAH'>UAH</option>`;
-    selectTo = document.getElementById('selectTo').innerHTML += `<option value='UAH'>UAH</option>`;
-    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
-    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[25]['cc']}'>${data[25]['cc']}</option>`;
-    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
-    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[24]['cc']}'>${data[24]['cc']}</option>`;
-    selectFrom = document.getElementById('selectFrom').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
-    selectTo = document.getElementById('selectTo').innerHTML += `<option value='${data[32]['cc']}'>${data[32]['cc']}</option>`;
-}
 
 const Rate = () => {
 
-    const [from, setFrom] = useState(0);
-    const [to, setTo] = useState(0);
+    const [from, setFrom] = useState();
+    const [to, setTo] = useState();
+    const [rates, setRates] = useState();
+    const [fromCC, setFromCC] = useState('UAH');
+    const [toCC, setToCC] = useState('UAH');
 
-    let rate = null;
+    const filterRates = (json) => {
+        return json.filter((item) => {
+            if (item.cc === 'USD' || item.cc === 'EUR' || item.cc === 'GBP') {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    useEffect(() => {
+        if (!rates) {
+            fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+                .then(data => data.json())
+                .then((data) => {
+                    let rates = [{cc: 'UAH', rate: 1.0}]
+                    rates = rates.concat(filterRates(data));
+                    setRates(rates);
+                });
+        }
+    });
+
+    let ccs = ['UAH'];
     if (rates) {
-        rate = rates[1].rate;
+        ccs = ccs.concat(rates.map((rate) => rate.cc));
+    }
+
+    console.log(rates)
+
+    const getRate = () => {
+        const rateFrom = rates.filter((rate) => rate.cc === fromCC)[0].rate;
+        const rateTo = rates.filter((rate) => rate.cc === toCC)[0].rate;
+        console.log(rateFrom);
+        console.log(rateTo);
+        return rateFrom/rateTo;
     }
 
     const handleFromChange = (value) => {
         setFrom(value);
+        const rate = getRate();
         setTo((value * rate).toFixed(2));
-        
     }
 
     const handleToChange = (value) => {
         setTo(value);
+        const rate = getRate();
+        setFrom((value * rate).toFixed(2));
+    }
+
+    const getOptions = () => {
+        return ccs.map((cc, index) => {
+            return <option key={index}>{cc}</option>;
+        });
+    }
+
+    const onFromCCChanged = (value) => {
+        setFromCC(value);
+        const rate = getRate();
+        setTo((from * rate).toFixed(2));
+    }
+
+    const onToCCChanged = (value) => {
+        setToCC(value);
+        const rate = getRate();
         setFrom((value * rate).toFixed(2));
     }
 
@@ -61,14 +82,26 @@ const Rate = () => {
                     value={from}
                     onChange={(e) => handleFromChange(e.target.value)}
                 ></input>
-                <select id='selectFrom' className={classes.selectFrom}></select>
+                <select 
+                    className={classes.selectFrom}
+                    onChange={(e) => onFromCCChanged(e.target.value)}
+                    value={fromCC}
+                >
+                    {getOptions()}
+                </select>
             </div>
             <div className={classes.toCurrencyBlock}>
                 <input id='inputTo' placeholder='0' type='number' className={classes.inputTo}
                     value={to}
                     onChange={(e) => handleToChange(e.target.value)}
                 ></input>
-                <select id='selectTo' className={classes.selectTo}></select>
+                <select
+                    className={classes.selectTo}
+                    onChange={(e) => onToCCChanged(e.target.value)}
+                    value={toCC}
+                >
+                    {getOptions()}
+                </select>
             </div>
             <div className={classes.equals}>=</div>
         </div>
