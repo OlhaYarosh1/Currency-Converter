3ed2d5b2de39fffb2e6539f17bb1a6a86d39aa39
Olha Yarosh <yarosholha1@gmail.com>
2022-09-20 13:32:27 +0300

    Custom Hook.

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,42 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import classes from './Box.module.css'
 import Header from './Header/Header'
 import Rate from './Rate/Rate'
+import useFetch from './useFetch/useFetch'
 
 const Box = () => {
 
-    const [rates, setRates] = useState();
-    let [exchangeDate, setExchangeDate] = useState('');
-    let [rateUSD, setRateUSD] = useState('');
-    let [rateEUR, setRateEUR] = useState('');
-
-
-    const filterRates = (json) => {
-        return json.filter((item) => {
-            if (item.cc === 'USD' || item.cc === 'EUR' || item.cc === 'GBP') {
-                return true;
-            }
-            return false;
-        });
-    }
-
-    useEffect(() => {
-        if (!rates) {
-            fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-                .then(data => data.json())
-                .then((data) => {
-                    let rates = [{cc: 'UAH', rate: 1.0}]
-                    rates = rates.concat(filterRates(data));
-                    const eurRate = rates.find((item) => item.cc === 'EUR')
-                    setExchangeDate(eurRate.exchangedate);
-                    let rateEUR = rates.find((item) => item.cc === 'EUR')
-                    setRateEUR(rateEUR.rate.toFixed(2));
-                    let rateUSD = rates.find((item) => item.cc === 'USD')
-                    setRateUSD(rateUSD.rate.toFixed(2));
-                    setRates(rates);
-                });
-        }
-    });
+    const [rates, rateUSD, rateEUR, exchangeDate] = useFetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
 
     return (
         <div className={classes.box}>
---
+++ src/Box/useFetch/useFetch.js
@@ -0,0 +1,39 @@
+import {useState, useEffect} from 'react'
+
+const useFetch = () => {
+
+    const [rates, setRates] = useState();
+    let [rateUSD, setRateUSD] = useState('');
+    let [rateEUR, setRateEUR] = useState('');
+    let [exchangeDate, setExchangeDate] = useState('');
+
+    const filterRates = (json) => {
+        return json.filter((item) => {
+            if (item.cc === 'USD' || item.cc === 'EUR' || item.cc === 'GBP') {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    useEffect(() => {
+
+        fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        .then(data => data.json())
+        .then((data) => {
+            let rates = [{cc: 'UAH', rate: 1.0}]
+            rates = rates.concat(filterRates(data));
+            const eurRate = rates.find((item) => item.cc === 'EUR')
+            setExchangeDate(eurRate.exchangedate);
+            let rateEUR = rates.find((item) => item.cc === 'EUR')
+            setRateEUR(rateEUR.rate.toFixed(2));
+            let rateUSD = rates.find((item) => item.cc === 'USD')
+            setRateUSD(rateUSD.rate.toFixed(2));
+            setRates(rates);
+        });
+    }, 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        
+        return [rates, rateUSD, rateEUR, exchangeDate];
+}
+
+export default useFetch
